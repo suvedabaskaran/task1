bf8b21d4104de58fac9a0a0e42253ded2c5a6574
suvedabaskaran <suvedabaskaran@gmail.com>
2025-01-25 19:36:44 +0530

    File uploading

---
+++ loop.py
@@ -0,0 +1,161 @@
+##sum of numbers in a given range
+##a = int(input('enter a number:'))
+##b = int(input('enter a number:'))
+##result = 0
+##for i in range(a,b+1):
+##    result+=i
+##print(result)
+
+##factorial of a number
+##a = int(input('enter a number:'))
+##factorial = 1
+##for i in range(1,a+1):
+##    factorial*=i
+##print(factorial)
+
+##sum of digits of a number
+##a = input('enter a number:')
+##result = 0
+##for i in a:
+##    result = result + int(i)
+##    print(result)
+
+##
+##a = int(input('enter a number:'))
+##b = int(input('enter a number:'))
+##result=0
+##for i in range(a,b,2):
+##    result=i
+##    print(result)
+
+##all even numbers within the given range
+##a = int(input('enter a number:'))
+##b = int(input('enter a number:'))
+##for i in range(a,b+1):
+##    if i%2==0:
+##        print(i)
+
+##calculate the sum of all odd numbers within the given range
+##a = int(input('enter a number:'))
+##b = int(input('enter a number:'))
+##result = 0
+##for i in range(a,b+1):
+##    if i%2!=0:
+##        result+=i
+##print(result)
+
+##print a multiplication table of a given number    
+##a = int(input('enter a number:'))
+##for i in range(1,11):
+##    print(i,'X',a,'=',i*a)
+
+##display numbers from a list
+##a = [1,2,3,4,5]
+##for i in a:
+##    print(i)
+
+##count the total number of digits in a number
+##a = input('enter a number:')
+##for i in a:
+##    num = str(len(a))
+##print(num)
+
+##check if the given string is a palindrome
+##a = input('enter a string:')
+##reverse=""
+##for i in a:
+##    reverse = i+reverse
+##    if a==reverse:
+##        print(a,'is a palindrome')
+##    else:
+##        print(a,'is not a palindrome')
+
+##accepts the word from the user and reverses it
+##a = input('enter a string:')
+##reverse = ""
+##for i in a:
+##    reverse = i + reverse
+##print(reverse)
+
+##count the number of even and odd numbers from a series of a numbers
+##a =[1,2,3,4,5,6,7,8,9,10]
+##even = 0
+##odd = 0
+##for i in a:
+##    if i%2==0:
+##        even+=1
+##    else:
+##         odd+=1
+##print('even number',even)
+##print('odd number',odd)
+
+##pattern
+##a = int(input('enter a number:'))
+##for i in range(a+1):
+##    for j in range(i):
+##        print('*',end='')
+##    print()
+
+##a = int(input('enter a number:'))
+##for i in range(a,0,-1):
+##    for j in range(i):
+##        print('*',end='')
+##    print()
+
+##convert the month name to number of days
+##month = input('enter the name of the month:')
+##if month in ('January','March','May','July','August','October','December'):
+##    print('has 31 days')
+##elif month in('February'):
+##    print('has 28/29 days')
+##else:
+##    print('has 30 days')
+
+##perfect number
+##a = int(input('enter a number:'))
+##result = 0
+##for i in range(1,a):
+##    if a%i==0:
+##        result+=i
+##if result == a:
+##    print(a,'is a perfect number')
+##else:
+##    print(a,'is not a perfect number')
+
+##count vowels in a string
+##x = input('enter a string:')
+##result = 0
+##vowels =('a','e','i','o','u')
+##for i in x:
+##    if i in vowels:
+##        result+=1
+##print(result)
+
+##calculate the number of digits and letters in a string
+##a = input('enter a string:')
+##d = ('0','1','2','3','4','5','6','7','8','9')
+##l = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+##digits = 0
+##letters = 0
+##for i in a:
+##    if i in d:
+##        digits += 1
+##    else:
+##        letters += 1
+##print('total digits in a string',digits)
+##print('total letters in a string',letters)
+
+##power of a number
+##a = int(input('enter a number:'))
+##b = int(input('give a power to the number:'))
+##print(a**b)
+        
+##check if the number is prime number
+
+
+
+    
+    
+    
+
+    
